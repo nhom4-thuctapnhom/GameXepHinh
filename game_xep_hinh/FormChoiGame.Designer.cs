ed4e0c85d21ac516c0bca2cf0f06040d1dc24fe7
Your Name <you@example.com>
2019-05-13 22:59:17 +0700

    initilize project

---
+++ game_xep_hinh/FormChoiGame.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace game_xep_hinh
+{
+    partial class FormChoiGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbDiem = new System.Windows.Forms.Label();
+            this.lbMucChoi = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbThoiGian = new System.Windows.Forms.Label();
+            this.btnTamDung = new System.Windows.Forms.Button();
+            this.btnTroChoiMoi = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(33, 31);
+            this.pb1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(133, 123);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(175, 31);
+            this.pb2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(133, 123);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb2.TabIndex = 0;
+            this.pb2.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(316, 31);
+            this.pb3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(133, 123);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb3.TabIndex = 0;
+            this.pb3.TabStop = false;
+            // 
+            // pb4
+            // 
+            this.pb4.Location = new System.Drawing.Point(33, 161);
+            this.pb4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(133, 123);
+            this.pb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb4.TabIndex = 0;
+            this.pb4.TabStop = false;
+            this.pb4.Click += new System.EventHandler(this.pb4_Click);
+            // 
+            // pb5
+            // 
+            this.pb5.Location = new System.Drawing.Point(175, 161);
+            this.pb5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(133, 123);
+            this.pb5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb5.TabIndex = 0;
+            this.pb5.TabStop = false;
+            // 
+            // pb6
+            // 
+            this.pb6.Location = new System.Drawing.Point(316, 161);
+            this.pb6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(133, 123);
+            this.pb6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb6.TabIndex = 0;
+            this.pb6.TabStop = false;
+            // 
+            // pb7
+            // 
+            this.pb7.Location = new System.Drawing.Point(33, 292);
+            this.pb7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(133, 123);
+            this.pb7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb7.TabIndex = 0;
+            this.pb7.TabStop = false;
+            // 
+            // pb8
+            // 
+            this.pb8.Location = new System.Drawing.Point(175, 292);
+            this.pb8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(133, 123);
+            this.pb8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb8.TabIndex = 0;
+            this.pb8.TabStop = false;
+            // 
+            // pb9
+            // 
+            this.pb9.Location = new System.Drawing.Point(316, 292);
+            this.pb9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(133, 123);
+            this.pb9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb9.TabIndex = 0;
+            this.pb9.TabStop = false;
+            // 
+            // pbMain
+            // 
+            this.pbMain.Location = new System.Drawing.Point(496, 123);
+            this.pbMain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(400, 369);
+            this.pbMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMain.TabIndex = 0;
+            this.pbMain.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(491, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Điểm:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(703, 31);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mức chơi:";
+            // 
+            // lbDiem
+            // 
+            this.lbDiem.AutoSize = true;
+            this.lbDiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDiem.ForeColor = System.Drawing.Color.Black;
+            this.lbDiem.Location = new System.Drawing.Point(565, 31);
+            this.lbDiem.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbDiem.Name = "lbDiem";
+            this.lbDiem.Size = new System.Drawing.Size(54, 25);
+            this.lbDiem.TabIndex = 1;
+            this.lbDiem.Text = "điểm";
+            // 
+            // lbMucChoi
+            // 
+            this.lbMucChoi.AutoSize = true;
+            this.lbMucChoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMucChoi.ForeColor = System.Drawing.Color.Black;
+            this.lbMucChoi.Location = new System.Drawing.Point(800, 31);
+            this.lbMucChoi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbMucChoi.Name = "lbMucChoi";
+            this.lbMucChoi.Size = new System.Drawing.Size(90, 25);
+            this.lbMucChoi.TabIndex = 1;
+            this.lbMucChoi.Text = "mức chơi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(491, 82);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Thời gian:";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbThoiGian
+            // 
+            this.lbThoiGian.AutoSize = true;
+            this.lbThoiGian.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbThoiGian.ForeColor = System.Drawing.Color.Black;
+            this.lbThoiGian.Location = new System.Drawing.Point(601, 82);
+            this.lbThoiGian.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbThoiGian.Name = "lbThoiGian";
+            this.lbThoiGian.Size = new System.Drawing.Size(85, 25);
+            this.lbThoiGian.TabIndex = 1;
+            this.lbThoiGian.Text = "thời gian";
+            // 
+            // btnTamDung
+            // 
+            this.btnTamDung.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnTamDung.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnTamDung.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTamDung.Location = new System.Drawing.Point(33, 443);
+            this.btnTamDung.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTamDung.Name = "btnTamDung";
+            this.btnTamDung.Size = new System.Drawing.Size(133, 49);
+            this.btnTamDung.TabIndex = 2;
+            this.btnTamDung.Text = "Tạm dừng";
+            this.btnTamDung.UseVisualStyleBackColor = false;
+            // 
+            // btnTroChoiMoi
+            // 
+            this.btnTroChoiMoi.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnTroChoiMoi.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnTroChoiMoi.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTroChoiMoi.Location = new System.Drawing.Point(175, 443);
+            this.btnTroChoiMoi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTroChoiMoi.Name = "btnTroChoiMoi";
+            this.btnTroChoiMoi.Size = new System.Drawing.Size(133, 49);
+            this.btnTroChoiMoi.TabIndex = 2;
+            this.btnTroChoiMoi.Text = "Trò chơi mới";
+            this.btnTroChoiMoi.UseVisualStyleBackColor = false;
+            this.btnTroChoiMoi.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnThoat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnThoat.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(316, 443);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(133, 49);
+            this.btnThoat.TabIndex = 2;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // FormChoiGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(912, 507);
+            this.Controls.Add(this.btnTroChoiMoi);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnTamDung);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbMucChoi);
+            this.Controls.Add(this.lbThoiGian);
+            this.Controls.Add(this.lbDiem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbMain);
+            this.Controls.Add(this.pb9);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb8);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb7);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FormChoiGame";
+            this.Text = "XẾP HÌNH";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pbMain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbDiem;
+        private System.Windows.Forms.Label lbMucChoi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbThoiGian;
+        private System.Windows.Forms.Button btnTamDung;
+        private System.Windows.Forms.Button btnTroChoiMoi;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ game_xep_hinh/FormChoiGame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game_xep_hinh
+{
+    public partial class FormChoiGame : Form
+    {
+        public FormChoiGame()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
