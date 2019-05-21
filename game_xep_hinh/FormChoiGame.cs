e0db800e556f404e12487b5c99f39e63943b2501
Your Name <you@example.com>
2019-05-21 09:21:31 +0700

    Thêm các biến và hàm khởi tạo ban đầu

--- game_xep_hinh/FormChoiGame.cs
+++ game_xep_hinh/FormChoiGame.cs
@@ -17,34 +17,49 @@ namespace game_xep_hinh
             InitializeComponent();
         }
 
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
+        public int x, y;
+        public int[] Arr = new int[9];
+        public int Diem;
+        public int Time;
+        public int Level;
+        public bool TT_Game = true; // Biến trạng thái game. Game chạy biến = true, game tạm dừng biến = false
 
+        public void initArr()
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                Arr[i] = i;
+            }
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        public void init()
         {
-
+            x = 0;
+            y = 0;
+            Diem = 0;
+            Time = 0;
+            Level = 0;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public int GetVT(int x, int y)
         {
-
+            return x + 3 * y;
         }
 
-        private void btnThoat_Click(object sender, EventArgs e)
+        public void GetAnh(PictureBox pb, int a)
         {
-            this.Close();
-        }
-
-        private void pb4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pb1_Click(object sender, EventArgs e)
-        {
-
+            switch(a)
+            {
+                case 1: pb.Image = game_xep_hinh.Properties.Resources.Anh1; break;
+                case 2: pb.Image = game_xep_hinh.Properties.Resources.Anh2; break;
+                case 3: pb.Image = game_xep_hinh.Properties.Resources.Anh3; break;
+                case 4: pb.Image = game_xep_hinh.Properties.Resources.Anh4; break;
+                case 5: pb.Image = game_xep_hinh.Properties.Resources.Anh5; break;
+                case 6: pb.Image = game_xep_hinh.Properties.Resources.Anh6; break;
+                case 7: pb.Image = game_xep_hinh.Properties.Resources.Anh7; break;
+                case 8: pb.Image = game_xep_hinh.Properties.Resources.Anh8; break;
+                case 9: pb.Image = null; pb.BackColor = Color.White; break;
+            }
         }
     }
 }
